7cc1b92f21f355ae0de9cbafd6fe56f8ceb214c0
yujen77300 <54500773+yujen77300@users.noreply.github.com>
2022-10-14 00:11:58 +0800

    feat : countdown function

--- Week-4/public/javascripts/script.js
+++ Week-4/public/javascripts/script.js
@@ -4,3 +4,19 @@ function myFunction() {
   var theUrl = "http://127.0.0.1:3000/square/".concat(e);
   window.location.replace(theUrl);
 }
+
+let seconds = 10;
+
+function countdown() {
+  seconds = seconds - 1;
+  if (seconds < 0) {
+    window.location = "/";
+  } else {
+    // 更新剩餘秒數
+    document.getElementById("countdown").innerHTML = seconds;
+    // 倒數
+    window.setTimeout("countdown()", 1000);
+  }
+}
+
+countdown();
